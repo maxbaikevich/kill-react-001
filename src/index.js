80de80b470d453947cd4597dd8137b46d2947e46
Maxim Baykevich <max.baikevich@gmail.com>
2020-06-22 15:39:02 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-
+const getResource = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url} + received ${res.status}`);
+  }
+  const body = await res.json();
+  return body;
+};
+getResource("https://swapi.dev/api/people/1/")
+  .then((body) => {
+    console.log(body);
+  })
+  .catch((err) => {
+    console.error("Could not fetch", err);
+  });
